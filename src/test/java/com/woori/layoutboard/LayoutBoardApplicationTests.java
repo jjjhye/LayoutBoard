422030f5d3ba54b10915da744fdee7a2745998bf
jjjhye <sojjh07@naver.com>
2024-07-10 12:04:27 +0900

    1. 작업틀

---
+++ src/test/java/com/woori/layoutboard/LayoutBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woori.layoutboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayoutBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
